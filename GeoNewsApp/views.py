796d53db8ac5ff7b3a992283319d962c9055eecf
kenne88 <kenne6912@gmail.com>
2021-06-12 17:47:18 -0400

    Fully operation, bug-free Marker Lists Now Working
    No longer creates duplicate location markers, and when user's add markers, only adds it to their list rather than all user's lists.

--- GeoNewsApp/views.py
+++ GeoNewsApp/views.py
@@ -1,5 +1,5 @@
 import pdb
-from accounts.models import Marker
+from accounts.models import Marker, User
 from django.http import HttpResponse
 from django.contrib import auth
 
@@ -12,13 +12,23 @@ def homepage(request):
         # Jordan, request.POST will contain a dictionary containing a key 'address'
         # With the value of whatever the input field was.
         target_address = request.POST['address']
-        newMarker = Marker(address = target_address)
-        newMarker.save()
-        request.user.markers.add(newMarker)
+        try:
+            newMarker = Marker.objects.get(address = target_address)    #Check to see if ANY marker ANYWHERE contains the address searched by user
+            try:
+                usermarker = User.objects.get(username = request.user.username, markers = newMarker)    #Check to see if THIS particular user already has that Address in their marker list
 
-        print(request.POST)
-        # pdb.set_trace()
-        # return
+            except request.user.DoesNotExist:   #if THIS particular user does not, add that marker to their list
+                request.user.markers.add(newMarker)
+                request.user.save()
+            
+        except Marker.DoesNotExist: #If marker doesn't exist ANYWHERE create it in list of markers, and add it to THIS user's marker list
+            newMarker = Marker(address = target_address)
+            newMarker.save()
+            request.user.markers.add(newMarker)
+            request.user.save()
+
+        
+        
     return render(request, 'homepage.html')
 
 # Django tutorial Count Homepage.
