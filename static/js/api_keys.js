2df4f0c87cdd6d8dcc1a834d3345c80e5fffb9c4
Phil Tracy <30930274+phitrac@users.noreply.github.com>
2021-07-06 11:15:30 -0400

    Delete static directory
    Unnecessary copy of 'static' folder and its children.

--- static/js/api_keys.js
+++
@@ -1,4 +0,0 @@
-// This will eventually be an encrypted/env file
-// Do not change anything here.
-
-// 7/3/2021 @2:17PM EST:  shifted to the use of env.  See: views.py, homepage.html, BingNewsFetch.js, google_maps.js
