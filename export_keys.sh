ecd86c217b0215d85d56d1a4069630e4d1d4d428
Phil Tracy <30930274+phitrac@users.noreply.github.com>
2021-07-06 12:42:08 -0400

    Add files via upload
    Bash script to make it easier to quickly export the API keys, and avoid input errors.  The keys inside can/should be replaced with the keys of the system owner.  The defaults in there now are current active keys for the CMSC 495 GeoNews project, but may not be active in the future. (7/6/2021)

---
+++ export_keys.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Export the Google and Bing API keys as env(s)
+
+# Place your own API key values here, replacing what you see with your own.
+GOOGLE_KEY=AIzaSyAJl8cHlF33w5ePQlxpfVxEh34Sow_uhac
+BING_KEY=a743e8ea9e8749089234756f0681648a
+
+export GOOGLE_API_KEY=GOOGLE_KEY
+export BING_API_KEY=BING_KEY
+echo "Google key set as:"
+echo $GOOGLE_KEY
+echo "Bing key set as:"
+echo $BING_KEY
+
