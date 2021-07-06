a2ac4a89adef1e59cbea17d54b46c4d46182102b
Phil Tracy <30930274+phitrac@users.noreply.github.com>
2021-07-06 16:17:36 -0400

    Update export_keys.sh
    Added a note about using 'source' to run the script.

--- export_keys.sh
+++ export_keys.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 # Export the Google and Bing API keys as env(s)
+# When running this script, precede with 'source' like such:
+# $ source ./export_keys.sh
+# This will ensure the parent shell you're working from will
+# have visbility into the env variables set.
 
 # Place your own API key values here, replacing what you see with your own.
 GOOGLE_KEY=AIzaSyAJl8cHlF33w5ePQlxpfVxEh34Sow_uhac
