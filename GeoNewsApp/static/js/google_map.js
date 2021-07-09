1dc4f23b8053fa449691f2b5833400efc9afb0aa
Alan Banks <58575743+alanbanks229@users.noreply.github.com>
2021-07-09 17:02:12 -0400

    removed unnecessary file (autocomplete.js) and also removed poor UI feedback for the scenario when a user physically enters a location themselves and still receive an alert saying 'no valid information available' when that is false

--- GeoNewsApp/static/js/google_map.js
+++ GeoNewsApp/static/js/google_map.js
@@ -20,98 +20,95 @@ let autocomplete;
 let info_toggle = false;
 
 window.initMap = function() {
+    let num_of_bookmarks = window.USER_BOOKMARKS.length;
+    if (num_of_bookmarks > 0 ){
+      map_object = returnAndCenterMapToLatestLocation()
+      locateAndCreateAllMarkerEvents(window.USER_BOOKMARKS, map_object, float_regex)
+    } else {
+      map_object = new google.maps.Map(document.getElementById('map'), {
+        // Keep in MIND:
+        // N and E coordinates are positive values
+        // W and S coordinates are negative values
+    
+        // Washington, D.C. Coordinates
+        center: {lat: 38.889248, lng: -77.050636}, 
+        zoom: 11
+      });
 
-  let num_of_bookmarks = window.USER_BOOKMARKS.length;
-  if (num_of_bookmarks > 0 ){
-    map_object = returnAndCenterMapToLatestLocation()
-    locateAndCreateAllMarkerEvents(window.USER_BOOKMARKS, map_object, float_regex)
-  } else {
-    map_object = new google.maps.Map(document.getElementById('map'), {
-      // Keep in MIND:
-      // N and E coordinates are positive values
-      // W and S coordinates are negative values
-  
-      // Washington, D.C. Coordinates
-      center: {lat: 38.889248, lng: -77.050636}, 
-      zoom: 11
+    }
+    //helper method
+    setup_autocomplete();
+
+    const geocoder = new google.maps.Geocoder();
+
+    // THIS IS ESSENTIALLY A PRE-CHECK TO SEE IF THE PROVIDED ADDRESS
+    // CAN PROPERLY BE APPLIED TO THE GOOGLE MAPS API, and create a marker.
+    document.getElementById("pseudo_submit").addEventListener("click", (e) => {
+      const address = document.getElementById("target_address").value;
+
+      geocoder.geocode({ address: address }, (results, status) => {
+        if (status === "OK") {
+          let target_coordinates = results[0].geometry.location;
+          // Adding this to a hidden element on HTML
+          document.getElementById("target_coords").value = target_coordinates;
+          document.getElementById("address_form").submit();
+        } else {
+          alert("Address Provided is not valid: " + status);
+        }
+      });
     });
 
-  }
-  //helper method
-  setup_autocomplete();
-
-
-  const geocoder = new google.maps.Geocoder();
-
-  
-  // THIS IS ESSENTIALLY A PRE-CHECK TO SEE IF THE PROVIDED ADDRESS
-  // CAN PROPERLY BE APPLIED TO THE GOOGLE MAPS API, and create a marker.
-  document.getElementById("pseudo_submit").addEventListener("click", (e) => {
-    const address = document.getElementById("target_address").value;
-
-    geocoder.geocode({ address: address }, (results, status) => {
-      if (status === "OK") {
-        let target_coordinates = results[0].geometry.location;
-        // Adding this to a hidden element on HTML
-        document.getElementById("target_coords").value = target_coordinates;
-        document.getElementById("address_form").submit();
-      } else {
-        alert("Address Provided is not valid: " + status);
+    // Prevents a user to submit the form accidently while clicking enter when
+    // choosing a auto-filled address.
+    // I think this is fine from a UI standpoint lemme know your thoughts --Alan
+    let input_field = document.getElementById("target_address");
+    input_field.addEventListener("keydown", (e) => {
+      if (e.key === "Enter"){
+        e.preventDefault();
+        //Prevents form to submit when user clicks enter within input field
       }
-    });
-  });
+    })
 
-  // Prevents a user to submit the form accidently while clicking enter when
-  // choosing a auto-filled address.
-  // I think this is fine from a UI standpoint lemme know your thoughts --Alan
-  let input_field = document.getElementById("target_address");
-  input_field.addEventListener("keydown", (e) => {
-    if (e.key === "Enter"){
-      e.preventDefault();
-      //Prevents form to submit when user clicks enter within input field
-    }
-  })
+    let bookmark_checkbox = document.getElementById("bookmark_checkbox_listener");
+    let bookmark_form = document.getElementById("Bookmarks_SideBar");
+    let bookmark_button_label = document.getElementById("show_bookmark_label");
+    bookmark_checkbox.addEventListener("change", (e) => {
+      console.log(bookmark_checkbox.checked)
+      //"this" is referring to the bookmark_checkbox html element ... not the javascript window.init
+      if (bookmark_checkbox.checked){
+        bookmark_checkbox.disabled = true;
+        setTimeout(function(){bookmark_checkbox.disabled = false;},1000);
+        bookmark_button_label.innerHTML = "Hide Bookmarks"
+        // bookmark_button_label.style.marginTop = "4rem";
+        // bookmark_button_label.style.marginBottom = "4rem";
+        bookmark_form.className = 'sidebarVisible';
+        bookmark_form.style.display = 'block';
+      } else {
+        // setTimeout(function(){bookmark_checkbox.disabled = false;},1000);
+        bookmark_button_label.innerHTML = "Show Bookmarks"
+        // bookmark_button_label.style.marginTop = "4rem";
+        // bookmark_button_label.style.marginBottom = "4rem";
+        bookmark_form.className = 'sidebarHidden';
+        bookmark_form.style.display = 'none';
+      }
+    })
 
-  let bookmark_checkbox = document.getElementById("bookmark_checkbox_listener");
-  let bookmark_form = document.getElementById("Bookmarks_SideBar");
-  let bookmark_button_label = document.getElementById("show_bookmark_label");
-  bookmark_checkbox.addEventListener("change", (e) => {
-    console.log(bookmark_checkbox.checked)
-    //"this" is referring to the bookmark_checkbox html element ... not the javascript window.init
-    if (bookmark_checkbox.checked){
-      bookmark_checkbox.disabled = true;
-      setTimeout(function(){bookmark_checkbox.disabled = false;},1000);
-      bookmark_button_label.innerHTML = "Hide Bookmarks"
-      // bookmark_button_label.style.marginTop = "4rem";
-      // bookmark_button_label.style.marginBottom = "4rem";
-      bookmark_form.className = 'sidebarVisible';
-      bookmark_form.style.display = 'block';
-    } else {
-      // setTimeout(function(){bookmark_checkbox.disabled = false;},1000);
-      bookmark_button_label.innerHTML = "Show Bookmarks"
-      // bookmark_button_label.style.marginTop = "4rem";
-      // bookmark_button_label.style.marginBottom = "4rem";
-      bookmark_form.className = 'sidebarHidden';
-      bookmark_form.style.display = 'none';
-    }
-  })
-
-  let info_icon = document.getElementById('info_icon');
-  let help_container = document.getElementById("info_help_container");
-  help_container.style.display = 'none' //For some reason the space infographic takes up is showing up, removing it on render
-  info_icon.addEventListener('click', (e) => {
-    info_toggle = !info_toggle;
-    if (info_toggle == true){
-      help_container.className = 'infoBarVisible';
-      help_container.style.display = 'block';
-    } else {
-      help_container.className = 'infoBarHidden';
-      setTimeout( () => {
-        help_container.style.display = 'none';
-      }, 250)
-  }})
-
+    let info_icon = document.getElementById('info_icon');
+    let help_container = document.getElementById("info_help_container");
+    help_container.style.display = 'none' //For some reason the space infographic takes up is showing up, removing it on render
+    info_icon.addEventListener('click', (e) => {
+      info_toggle = !info_toggle;
+      if (info_toggle == true){
+        help_container.className = 'infoBarVisible';
+        help_container.style.display = 'block';
+      } else {
+        help_container.className = 'infoBarHidden';
+        setTimeout( () => {
+          help_container.style.display = 'none';
+        }, 250)
+    }})
 };
+// End of window.initMap() 
 
 
 function setup_autocomplete(){
@@ -135,19 +132,36 @@ function setup_autocomplete(){
     map_object,
     anchorPoint: new google.maps.Point(0, -29),
   });
+
   autocomplete.addListener("place_changed", () => {
     infowindow.close();
     // marker.setVisible(false);
+    let available_matches = document.getElementsByClassName("pac-item-query") //[0].textContent;
     let place = autocomplete.getPlace();
-    if (!place.geometry || !place.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
+    // console.log("inside google_map.js debugger\n", first_closest_match, "\n=?=\n", place.name);
+    // invokeDebugger(autocomplete);
+    // console.log("(", !place.geometry, ") &&", "(", first_closest_match.toString(), "!=", place.name.toString(), ")" );
+    for(let iteration in available_matches){
+      console.log(
+        !place.geometry && 
+        (
+          available_matches[iteration].textContent.toString() != place.name.toString()
+        )
+      );
+      if (available_matches[iteration].textContent.toString() == place.name.toString()){
+        //user selected or typed in a valid match
+        infowindowContent.children["place-name"].textContent = place.name;
+        infowindowContent.children["place-address"].textContent = place.formatted_address;
+        infowindow.open(map_object, marker);
+        return;
+      }
     }
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent = place.formatted_address;
-    infowindow.open(map_object, marker);
+    // User entered the name of a Place that was not suggested and
+    // pressed the Enter key, or the Place Details request failed.
+    window.alert("No details available for input: '" + place.name + "'");
+    // return;
+    // if ((!place.geometry) && (first_closest_match.toString() != place.name.toString())) {
+    // }
     
   });
 }
@@ -169,6 +183,7 @@ function returnAndCenterMapToLatestLocation(){
   })
   return mapObject;
 }
-
-// Append the 'script' element to 'head'
 document.head.appendChild(script);
+console.log("YEP")
+// Append the 'script' element to 'head'
+
